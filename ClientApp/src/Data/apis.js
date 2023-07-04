47510476f2c8572687b3ced35976cc4c20da80bd
rushsangs <rush.sang@rutgers.edu>
2023-07-04 15:40:31 -0600

    saving build

--- ClientApp/src/Data/apis.js
+++ ClientApp/src/Data/apis.js
@@ -3,8 +3,10 @@ import constraintsData from './constraints'
 import { sometimesAction } from './actions'
 
 
-export const getStoryData = () => {
-    return storyData
+export const getStoryData = async () => {
+    const response = await fetch('http://localhost:7273/weatherforecast');
+    const data = await response.json();
+    return data
 }
 
 export const getConstraintsData = () => {
@@ -14,3 +16,5 @@ export const getConstraintsData = () => {
 export const getNextDropdownData = () => {
     return sometimesAction
 }
+// const response = await fetch('weatherforecast');
+//     const data = await response.json();
