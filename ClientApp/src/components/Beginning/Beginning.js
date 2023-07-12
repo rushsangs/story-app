add3040056cf7487425341ef630a8a00bdb06966
rushsangs <rush.sang@rutgers.edu>
2023-07-12 11:29:37 -0600

    beginning and ending tabs replicate actions

--- ClientApp/src/components/Beginning/Beginning.js
+++ ClientApp/src/components/Beginning/Beginning.js
@@ -4,12 +4,40 @@ import { PlusSquareOutlined, MinusSquareOutlined } from "@ant-design/icons";
 import { List, Typography } from "antd";
 import constraintsData from "../../Data/constraints";
 import { getNextDropdownData } from "../../Data/apis";
+import InputRow from "../InputRow/InputRow";
 
-const Beginning = () => {
+const Beginning = ({componentId, dropdownComponents, onComponentChange, onDropdownChange}) => {
+  const handleAddDropdown = () => {
+    const newComponent = {
+      id: componentId,
+      component: (
+        <InputRow
+          key={componentId}
+          id={componentId}
+          onRemove={handleRemoveDropdown}
+        />
+      ),
+    };
+    onDropdownChange((prevComponents) => [
+      ...prevComponents,
+      newComponent,
+    ]);
+    onComponentChange((prevId) => prevId + 1);
+  };
+
+  const handleRemoveDropdown = (id) => {
+    onDropdownChange((prevComponents) =>
+      prevComponents.filter((comp) => comp.id !== id)
+    );
+  };
+
   return (
     <>
-      Hello Beginning!
+      <Space direction="vertical" style={{ padding: "0 15px" }}>
+        <Button onClick={handleAddDropdown}>Add Data</Button>
+        {dropdownComponents.map((obj) => obj.component)}
+      </Space>
     </>
   );
-};
+};  
 export default Beginning;
--- ClientApp/src/components/Ending/Ending.js
+++ ClientApp/src/components/Ending/Ending.js
@@ -4,13 +4,41 @@ import { PlusSquareOutlined, MinusSquareOutlined } from "@ant-design/icons";
 import { List, Typography } from "antd";
 import constraintsData from "../../Data/constraints";
 import { getNextDropdownData } from "../../Data/apis";
+import InputRow from "../InputRow/InputRow";
 
 
-const Ending = () => {
+const Ending =({componentId, dropdownComponents, onComponentChange, onDropdownChange}) => {
+  const handleAddDropdown = () => {
+    const newComponent = {
+      id: componentId,
+      component: (
+        <InputRow
+          key={componentId}
+          id={componentId}
+          onRemove={handleRemoveDropdown}
+        />
+      ),
+    };
+    onDropdownChange((prevComponents) => [
+      ...prevComponents,
+      newComponent,
+    ]);
+    onComponentChange((prevId) => prevId + 1);
+  };
+
+  const handleRemoveDropdown = (id) => {
+    onDropdownChange((prevComponents) =>
+      prevComponents.filter((comp) => comp.id !== id)
+    );
+  };
+
   return (
     <>
-      Hello Ending!
+      <Space direction="vertical" style={{ padding: "0 15px" }}>
+        <Button onClick={handleAddDropdown}>Add Data</Button>
+        {dropdownComponents.map((obj) => obj.component)}
+      </Space>
     </>
   );
-};
+};  
 export default Ending;
