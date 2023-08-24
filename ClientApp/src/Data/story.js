a3e4681bfbb7fe37de58f934d405c9bdf160b32f
rushsangs <rush.sang@rutgers.edu>
2023-08-24 00:47:21 -0600

    ready for pilots

--- ClientApp/src/Data/story.js
+++ ClientApp/src/Data/story.js
@@ -128,9 +128,24 @@ export const task3 = [
   ];
 
   export const task8 = [
-    {key: 0, text: "Teddy attempts and fails to heat the Soup using the Microwave.", status: 0, test:((x,y)=> x!== undefined && x.filter( step => step.includes("Teddy tries to start the microwave to heat up ")).length > 0)},
-    {key: 2, text: "Teddy heats up the soup using the stove.", status: 0, test:((x,y)=> x!== undefined && x.filter( step => step.includes("Teddy heats the Soup over the stove.")).length > 0)},
+    {key: 1, text: "Soup is not heated up initially.", status: 0, test:((x,y)=> {
+        if(y!== undefined)
+            for (let k in y){
+                // console.log(y[k]);
+                if(y[k].page==='beginning'&&y[k].group==="world"&& y[k].values.includes('heated Soup') && y[k].values.includes("False"))
+                    return true;
+            }
+        return false;
+    })},
+    {key: 2, text: "An action constraint specifies that Teddy heats up the soup using the stove.", status: 0, test:((x,y)=> {
+        if(y!== undefined)
+            for (let k in y)
+                if(y[k].page==='actions'&& y[k].values.includes("heatWithStove Soup Pot Teddy"))
+                    return true;
+        return false;
+    })},
     {key: 3, text: "Teddy eats the soup.", status: 0, test:((x,y)=> x!== undefined && x.filter( step => step.includes("Teddy eats the Soup")).length > 0)},
+    
   ];
 
   export const task9 = [
@@ -220,7 +235,7 @@ export const taskData = [
     },
     {
         taskNumber: 8,
-        taskInfo: "Using action constraints in the Actions tab, you can directly specify what actions you'd like to see in the story. Use an action constraint to specifically ask the system to find stories where Teddy uses the stovetop after failing to use the microwave.",
+        taskInfo: "Using action constraints in the Actions tab, you can directly specify what actions you'd like to see in the story. Try using action constraints to specifically guide Teddy to use the stovetop and the microwave to heat up the Soup.",
         tasks: task8
     },
     {
@@ -235,7 +250,7 @@ export const taskData = [
     },
     {
         taskNumber: 11,
-        taskInfo: "Use all you have learnt to create a story with both Poppy and Teddy achieving their goals, and Teddy failing to use the Microwave at some point.",
+        taskInfo: "Use all you have learnt to create a story with both Poppy and Teddy achieving their goals, and Teddy failing to use the Microwave at some point. Teddy and Poppy cannot start with incorrect beliefs. Specifically, create a story where Teddy enters the Kitchen after Poppy disconnects the microwave and fails to use the microwave initially because he still believed the microwave was plugged in.",
         tasks: task11
     }
 ]
