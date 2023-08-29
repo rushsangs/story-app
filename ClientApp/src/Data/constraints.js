309268ffb9d3a6fa44e80c04961ab8763227a213
rushsangs <rush.sang@rutgers.edu>
2023-08-28 22:00:01 -0600

    added task data for mocking for baa

--- ClientApp/src/Data/constraints.js
+++ ClientApp/src/Data/constraints.js
@@ -1,12 +1,103 @@
-export default [
-{
-    label: "Include sometime",
-    value: "Include sometime",
-    name:  "Include sometime"
-},
-{
-    label: "Don't include",
-    name:  "Include before",
-    value: "Don't include"
-}
+const task4 = [
+    {
+        "rowId": 3,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"Soup is in the fridge.\",\"value\":\"infridge Soup\",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"True\",\"value\":\"True\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"False\",\"value\":\"False\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 10,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"Soup has been cooked.\",\"value\":\"heated Soup\",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"True\",\"value\":\"True\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"False\",\"value\":\"False\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 2,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"The outlet is powering \",\"value\":\"The outlet is powering \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"the Microwave\",\"value\":\"the Microwave\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"the Toaster\",\"value\":\"the Toaster\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"nothing\",\"value\":\"nothing\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 3,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"The soup is in \",\"value\":\"The soup is in \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"a Bowl\",\"value\":\"a Bowl\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"a Pot\",\"value\":\"a Pot\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 4,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"The Soup has been eaten by \",\"value\":\"The Soup has been eaten by \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"nobody\",\"value\":\"nobody\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"Teddy\",\"value\":\"Teddy\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 5,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"Teddy is in the \",\"value\":\"Teddy is in the \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"Kitchen\",\"value\":\"Kitchen\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"LivingRoom\",\"value\":\"LivingRoom\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 6,
+        "page": "beginning",
+        "group": "Teddy",
+        "main_Dropdown": "[{\"label\":\"The outlet is powering \",\"value\":\"The outlet is powering \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"the Microwave\",\"value\":\"the Microwave\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"the Toaster\",\"value\":\"the Toaster\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"nothing\",\"value\":\"nothing\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    }
+]
+
+const task3= [
+    {
+        "rowId": 3,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"Soup is in the fridge.\",\"value\":\"infridge Soup\",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"True\",\"value\":\"True\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"False\",\"value\":\"False\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 10,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"Soup has been cooked.\",\"value\":\"heated Soup\",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"True\",\"value\":\"True\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"False\",\"value\":\"False\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 2,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"The outlet is powering \",\"value\":\"The outlet is powering \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"the Microwave\",\"value\":\"the Microwave\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"the Toaster\",\"value\":\"the Toaster\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"nothing\",\"value\":\"nothing\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 3,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"The soup is in \",\"value\":\"The soup is in \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"a Bowl\",\"value\":\"a Bowl\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"a Pot\",\"value\":\"a Pot\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 4,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"The Soup has been eaten by \",\"value\":\"The Soup has been eaten by \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"nobody\",\"value\":\"nobody\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"Teddy\",\"value\":\"Teddy\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 5,
+        "page": "beginning",
+        "group": "world",
+        "main_Dropdown": "[{\"label\":\"Teddy is in the \",\"value\":\"Teddy is in the \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"Kitchen\",\"value\":\"Kitchen\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"LivingRoom\",\"value\":\"LivingRoom\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    },
+    {
+        "rowId": 6,
+        "page": "beginning",
+        "group": "Teddy",
+        "main_Dropdown": "[{\"label\":\"The outlet is powering \",\"value\":\"The outlet is powering \",\"tooltip\":\"Select one:\",\"color\":\"\"}]",
+        "arguments": "[[{\"label\":\"the Microwave\",\"value\":\"the Microwave\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"the Toaster\",\"value\":\"the Toaster\",\"tooltip\":\"Select one:\",\"color\":\"\"},{\"label\":\"nothing\",\"value\":\"nothing\",\"tooltip\":\"Select one:\",\"color\":\"\"}]]"
+    }
 ]
